9a825b021ec7f3d9067b6f608656f679c3ec5ca8
Thomas Brandl <thobra@gmail.com>
2014-07-06 13:29:02 +0200

    Marks removed areas red, added green Changes displayed side by side

--- compare.py
+++ compare.py
@@ -4,24 +4,42 @@ import os, glob
 from time import sleep
 from SimpleCV import *
 
-current_images_path = './Screens'
-current_images_path = '/Users/thomasbrandl/Google Drive/Screens'
-previous_images_path = '/Users/thomasbrandl/Google Drive/Screens pre migration'
+base_images_path = os.path.expanduser('~')
+current_images_path = base_images_path + '/Google Drive/Screens'
+previous_images_path = base_images_path + '/Google Drive/Screens pre migration'
 mismatch_images_path = current_images_path + '/Mismatches'
 
-scale_factor = .5
-border_width = int(10 ** scale_factor)
+scale_factor = .75
+border_width = int(5 ** scale_factor)
+
+mismatch = []
+
+
+def mask(imgA, imgB):
+	tmp = (imgB - imgA)
+	return tmp
 
 def openImage(img):
 	try:
-		_img = Image(img)
+		_img = Image(img).scale(scale_factor)
 		return _img
 	except cv2.error as e:
-		print "Error opening image: %s" % cv2.error 
+		print "Error opening image: %s" % cv2.error
 
-mismatch = []
+def markBlobs(img, color=Color.RED):
+	# thresh 5 filters tiny changes we don't care about
+	# max and minsize is by trial
+	blobs = img.binarize(thresh=5).invert().findBlobs(
+				maxsize=((img.width*img.height)/2),
+				minsize=1000)
+	if blobs:
+		blobs.image = img
+		for b in blobs:
+			b.drawRect(color=color, width=border_width)
+	# merge the drawing layer on the image. Otherwise drawings get lost when changing the source image in any way
+	return img.applyLayers()
+
 for current_image_path in glob.glob('%s/*.png' % current_images_path):
-	#img = Image(screen)
 	current_image_name = os.path.basename(current_image_path)
 	previous_image_path = '%s/%s' % (previous_images_path, current_image_name)
 
@@ -31,43 +49,41 @@ for current_image_path in glob.glob('%s/*.png' % current_images_path):
 		print "opening previous ..."
 		imgB = openImage(previous_image_path).scale(scale_factor)
 		if imgA.width != imgB.width:
+			# Not sure why this happened, screenshots are taken at a defined width of 1600px
 			print "Width does not match! Continuing ..."
 			continue
 		print "comparing ..."
 		try:
-			imgD = imgB - imgA
+			imgD = mask(imgB, imgA)
 		except cv2.error as e:
-			print "Error comparing image: %s" % cv2.error 
+			print "Error comparing image: %s" % cv2.error
 			mismatch.append(current_image_name)
 		finally:
 			# when the images are not the same size, image arithmetric (adding, subtracting) does not work
-			# before we can compare them, the need the same image dimensions.
+			# before we can compare them, they need the same image dimensions.
 			diff_height = imgA.height - imgB.height
 			if diff_height < 0:
 				imgA = imgA.embiggen((imgA.width, imgB.height), Color.BLACK, (0,0))
 			elif diff_height > 0:
 				imgB = imgB.embiggen((imgA.width, imgA.height), Color.BLACK, (0,0))
-			# now try again to find the differences	
-			print imgA, imgB
-			imgD = imgB - imgA
+			# now try again to find the differences
+			imgD = mask(imgB, imgA)
 
-		blobs = imgD.binarize(thresh=0).erode().invert().findBlobs(maxsize=((imgD.width*imgD.height)/2), minsize=1000)
-		# Dim previous version by 50% and add the difference to it
-		imgX = imgB/2 + imgD
-		if blobs:
-			blobs.image = imgX
-			#blobs.draw(color=Color.GREEN, width=5)
-			for b in blobs:
-				b.drawRect(color=Color.RED, width=border_width)
+		# Substract A from B, then B from A and add them. This way, we can also see black areas
+		# that were added/removed
+		imgD = markBlobs(imgD, Color.RED)
+		imgE = markBlobs((imgA.invert() - imgB.invert()), Color.GREEN)
+		imgX = imgD + imgE
 
 		matrix = imgD.getNumpy()
 		mean = matrix.mean()
+
 		if mean > 0.0:
 			mismatch.append(current_image_name)
-			imgX.save('%s/%s' % (mismatch_images_path, current_image_name))
+			imgB.sideBySide(imgA) \
+			.sideBySide(imgX) \
+			.save('%s/%s.%s' % (mismatch_images_path, current_image_name[:-4], 'jpg'))
 		print mean
 
 print mismatch
-
-
 
