bbb168e85151adb99be4a7e8b726cee45aeb6872
Thomas Brandl <thobra@gmail.com>
2014-07-07 10:49:14 +0200

    Works from commandline

--- capture.py
+++ capture.py
@@ -1,39 +1,79 @@
 #!/usr/bin/python
 
+from os import path
 import urllib
 from xml.dom import minidom
 import re
 import subprocess
+import optparse
 
-sitemap = 'http://native:native@www.integration.native-instruments.de/en/sitemap'
-#sitemap = './sitemap.xml'
+__version__ = "0.1"
 
-dom = minidom.parse(urllib.urlopen(sitemap))
-#dom = minidom.parse(open(sitemap))
+options = {}
+args = []
 
-output_path = '/Users/thomasbrandl/Google Drive/Screens'
+def init():
+	global options, args
+	usage = """%prog [options] [http://example.net/ ...]
 
-locations = dom.getElementsByTagName('loc')
-urls = []
-for node in locations:
-	url = node.childNodes[0].nodeValue
-	# only capture pages 5 levels down (no subpages)
-	# de/ products/ komplete/ effects/ vintage-compressors/ = 5
-	# won't find pages
-	if re.search(r"\.de/([^/]+/){,5}$", url):
- 		urls.append(url)
-print len(urls)
+	Examples:
+	%prog http://www.ni.de/            	# screengrab the live site (not supported yet)
+	%prog -S --site-map		# Crawl Google sitemap, <url><loc>http://</loc></url>
+	%prog -O --output-path	# Save files to Screens folder, defaults to ~/
+	%prog -W --width		# width of the browser screen, defaults to 1280
+	%prog -L --levels		# How many url path levels to go down (/en/company/ is on level 2), defaults to 2"""
 
-##urls = ['http://www.integration.native-instruments.de/de/products/komplete/effects/premium-tube-series/']
+	cmdparser = optparse.OptionParser(usage, version=("capture " + __version__))
 
-for url in urls:
-	subprocess.call([	"webkit2png",
-						"--dir=%s" % (output_path),
-						"-F",
-						"-W 1600",
-						"--js='var i=0; function scroll(){ i += 600; if (i > document.height) { window.scrollTo(0,0);webkit2png.start() } else { window.scrollTo(0, i); window.setTimeout(scroll, 500)}}; webkit2png.stop(); scroll();'",
-						url
-					])
+	cmdparser.add_option("-O", "--output-path",
+						type="string",
+						default=path.expanduser("~"),
+						help=optparse.SUPPRESS_HELP)
+	cmdparser.add_option("-S", "--sitemap",
+						type="string",
+						help=optparse.SUPPRESS_HELP)
+	cmdparser.add_option("-L", "--levels", type="int",
+						default=5,
+						help=optparse.SUPPRESS_HELP)
+	cmdparser.add_option("-W", "--width",
+						type="int",
+						default=1280,
+						help=optparse.SUPPRESS_HELP)
+
+	(options, args) = cmdparser.parse_args()
+	# if len(args) == 0:
+	# 	cmdparser.print_usage()
+	# 	raise RuntimeError("Must specify a URL to capture screens from")
+
+def main():
+	global options, args
+	init()
+	dom = minidom.parse(urllib.urlopen(options.sitemap))
+	locations = dom.getElementsByTagName('loc')
+	urls = []
+
+	for node in locations:
+		url = node.childNodes[0].nodeValue
+		# only capture pages 5 levels down (no subpages)
+		# de/ products/ komplete/ effects/ vintage-compressors/ = 5
+		# won't find pages
+		reg = r"\.[a-z]{2,3}/([^/]+/){,%d}$" % options.levels
+		if re.search(reg, url):
+	 		urls.append(url)
+	print "Capturing %d urls" % len(urls)
+
+	##urls = ['http://www.integration.native-instruments.de/de/products/komplete/effects/premium-tube-series/']
+
+	for url in urls:
+		subprocess.call([	"webkit2png",
+							"--dir=%s" % (options.output_path),
+							"-F",
+							"-W " + str(options.width),
+							"--js='var i=0; function scroll(){ i += 600; if (i > document.height) { window.scrollTo(0,0);webkit2png.start() } else { window.scrollTo(0, i); window.setTimeout(scroll, 500)}}; webkit2png.stop(); scroll();'",
+							url
+						])
 
 
-
+if __name__ == '__main__':
+	main()
+	
